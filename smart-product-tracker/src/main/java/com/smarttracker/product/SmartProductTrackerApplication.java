d8d6ea851c4ef0e7974a9ce9e1ab0c0e3b06fe0e
Eren <nouman.laravel@outlook.com>
2025-12-03 18:12:01 +0800

    Initial commit: Spring Boot project setup

---
+++ smart-product-tracker/src/main/java/com/smarttracker/product/SmartProductTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.smarttracker.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartProductTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartProductTrackerApplication.class, args);
+	}
+
+}
---
+++ smart-product-tracker/src/test/java/com/smarttracker/product/SmartProductTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttracker.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartProductTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
